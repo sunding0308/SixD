88770d3fe79addcfe7b22c94c9c9551267077f5c
sunding <sunding1992@gmail.com>
2018-06-19 15:23:35 +0800

    add jpush service

---
+++ app/Services/JPushService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use JPush\Client as JPush;
+use Illuminate\Support\Facades\Log;
+
+class JPushService
+{
+    private $app_key; // appkey
+    private $master_secret; // master_secret
+
+    public function __construct()
+    {
+        $this->app_key = config('jpush.app_key');
+        $this->master_secret = config('jpush.master_secret');
+    }
+
+    public function push($registrationId)
+    {
+        $client = new JPush($this->app_key, $this->master_secret); // 实例化client.php中的client类
+
+        $push_payload = $client->push() // 调用push方法（返回一个PushPayload实例）
+            ->setPlatform('android') // 设置平台
+            ->addRegistrationId($registrationId) // 设置设备推送
+            ->message('Hello JPush'); // 设置推送通知内容
+        try {
+            $response = $push_payload->send(); // 执行推送
+        }catch (\JPush\Exceptions\APIConnectionException $e) { // 请求异常
+            Log::error($e);
+        } catch (\JPush\Exceptions\APIRequestException $e) { // 回复异常
+            Log::error($e);
+        }
+
+        Log::error($response); // 请求成功，返回信息
+    }
+}
---
+++ config/jpush.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'app_key' => env('APP_KEY'),
+    'master_secret' => env('MASTER_SECRET'),
+];
