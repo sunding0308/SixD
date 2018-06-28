ebf2e457576d218957155ddfd4c6e98317dbc3dd
sunding <sunding1992@gmail.com>
2018-06-28 12:23:11 +0800

    change back userRank resource

--- app/Http/Resources/UserRankResource.php
+++ app/Http/Resources/UserRankResource.php
@@ -15,17 +15,27 @@ class UserRankResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'data' => [
-                'id' => $this->id,
-                'machine_id' => $this->machine_id,
-                'user_id' => $this->user_id,
-                'user_nickname' => $this->user_nickname,
-                'rank' => $this->rank,
-                'machine_rank' => $this->machine_rank,
-                'created_at' => $this->created_at->timestamp,
-                'updated_at' => $this->updated_at->timestamp
-            ],
-            'status' => 'success'
+            'id' => $this->id,
+            'machine_id' => $this->machine_id,
+            'user_id' => $this->user_id,
+            'user_nickname' => $this->user_nickname,
+            'rank' => $this->rank,
+            'machine_rank' => $this->machine_rank,
+            'created_at' => $this->created_at->timestamp,
+            'updated_at' => $this->updated_at->timestamp
+        ];
+    }
+
+    /**
+     * 返回应该和资源一起返回的其他数据数组。
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'status' => 'success',
         ];
     }
 }
