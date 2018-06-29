1319d2acf9712bb2aea06675e148c32a27e81b0d
sunding <sunding1992@gmail.com>
2018-06-29 15:16:33 +0800

    add laravel-api-auth

---
+++ config/api_auth.php
@@ -0,0 +1,25 @@
+<?php
+
+use SunD1ng\LaravelApiAuth\Middleware;
+
+return [
+    'status' => Middleware::STATUS_ON, // 状态，LaravelApiAuth::STATUS_ON  或者 LaravelApiAuth::STATUS_OFF
+
+    'roles' => [
+               'Nhnp07YAQXGGKiezHLRV77P36d7Azg1L' => [
+                   'name' => '6d',        // 角色名字，例如 android
+                   'secret_key' => '35qcSqCVYSNBC7zIgdK6RShyi67UEcq0',
+               ],
+    ],
+
+    'signature_methods' => [
+        'md5' => \SunD1ng\LaravelApiAuth\Signatures\Md5::class,
+    ],
+
+    'skip' => [
+        'is' => [Middleware::class, 'default_excludes_handler'],
+        'urls'    => [],
+    ],
+
+    'timeout' => 60, // 签名失效时间，单位: 秒
+];
