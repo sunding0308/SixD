e0b9ae3791dd6f1915fa09355e5375a7f0918150
sunding <sunding1992@gmail.com>
2018-06-29 04:33:20 +0000

    Revert "add laravel-api-auth"
    This reverts commit f62175576f876a29d9b09cd118c2c7bfd866f2ad.

--- routes/api.php
+++ routes/api.php
@@ -16,35 +16,30 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/get_statistics_data', 'Api\ApiController@getStatisticsData');
 
-Route::group(['namespace'=>'Api'], function(){
-    Route::post('/get_statistics_data', 'ApiController@getStatisticsData');
+//push signal
+Route::get('/push_alarms_signal', 'Api\PushController@pushAlarmsSignal');
+Route::get('/push_overage_signal', 'Api\PushController@pushOverageSignal');
+Route::get('/push_hardware_status_signal', 'Api\PushController@pushHardwareStatusSignal');
+Route::get('/push_records_signal', 'Api\PushController@pushRecordsSignal');
+Route::get('/push_environment_signal', 'Api\PushController@pushEnvironmentSignal');
+Route::get('/push_water_quality_statistics_signal', 'Api\PushController@pushWaterQualityStatisticsSignal');
+Route::get('/push_app_menu_analysis_signal', 'Api\PushController@pushAppMenuAnalysisSignal');
+Route::get('/push_api_analysis_signal', 'Api\PushController@pushApiAnalysisSignal');
 
-    //push signal
-    Route::get('/push_alarms_signal', 'PushController@pushAlarmsSignal');
-    Route::get('/push_overage_signal', 'PushController@pushOverageSignal');
-    Route::get('/push_hardware_status_signal', 'PushController@pushHardwareStatusSignal');
-    Route::get('/push_records_signal', 'PushController@pushRecordsSignal');
-    Route::get('/push_environment_signal', 'PushController@pushEnvironmentSignal');
-    Route::get('/push_water_quality_statistics_signal', 'PushController@pushWaterQualityStatisticsSignal');
-    Route::get('/push_app_menu_analysis_signal', 'PushController@pushAppMenuAnalysisSignal');
-    Route::get('/push_api_analysis_signal', 'PushController@pushApiAnalysisSignal');
-
-    Route::group(['middleware'=>'api_auth'], function(){
-        Route::get('/check_status', 'OnlineController@checkStatus');
-        Route::get('/topup', 'TopupController@topup');
-        Route::get('/vip_topup', 'TopupController@vipTopup');
-        Route::get('/reset_overage', 'TopupController@resetOverage');
-        Route::post('/online', 'OnlineController@online');
-        Route::post('/online/user_rank', 'OnlineController@setUserRank');
-        Route::get('/online/user_rank', 'OnlineController@getUserRank');
-        Route::post('/alarms', 'AlarmsController@alarms');
-        Route::post('/realtime/overage', 'RealtimeController@overage');
-        Route::post('/report/hardware_status', 'ReportController@hardwareStatus');
-        Route::post('/report/records', 'ReportController@records');
-        Route::post('/report/environment', 'ReportController@environment');
-        Route::post('/report/water_quality_statistics', 'ReportController@waterQualityStatistics');
-        Route::post('/report/app_menu_analysis', 'ReportController@appMenuAnalysis');
-        Route::post('/report/api_analysis', 'ReportController@apiAnalysis');
-    });
-});
+Route::get('/check_status', 'Api\OnlineController@checkStatus');
+Route::get('/topup', 'Api\TopupController@topup');
+Route::get('/vip_topup', 'Api\TopupController@vipTopup');
+Route::get('/reset_overage', 'Api\TopupController@resetOverage');
+Route::post('/online', 'Api\OnlineController@online');
+Route::post('/online/user_rank', 'Api\OnlineController@setUserRank');
+Route::get('/online/user_rank', 'Api\OnlineController@getUserRank');
+Route::post('/alarms', 'Api\AlarmsController@alarms');
+Route::post('/realtime/overage', 'Api\RealtimeController@overage');
+Route::post('/report/hardware_status', 'Api\ReportController@hardwareStatus');
+Route::post('/report/records', 'Api\ReportController@records');
+Route::post('/report/environment', 'Api\ReportController@environment');
+Route::post('/report/water_quality_statistics', 'Api\ReportController@waterQualityStatistics');
+Route::post('/report/app_menu_analysis', 'Api\ReportController@appMenuAnalysis');
+Route::post('/report/api_analysis', 'Api\ReportController@apiAnalysis');
