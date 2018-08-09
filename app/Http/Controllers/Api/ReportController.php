32582235df7a1e097470c139be875d71461abfe1
sunding <sunding1992@gmail.com>
2018-08-09 15:58:08 +0800

    import PushRecord

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Machine;
+use App\PushRecord;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
