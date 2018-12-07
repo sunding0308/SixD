d80b7742f7cb64356d4e3f870cc1c965e4c5162d
SunD1ng <sunding1992@gmail.com>
2018-12-07 10:43:53 +0800

    test no sort debug file

--- app/Http/Controllers/Admin/MachineController.php
+++ app/Http/Controllers/Admin/MachineController.php
@@ -90,12 +90,13 @@ class MachineController extends Controller
 
     public function debug(Request $request, Machine $machine)
     {
-        $files = $this->paginate(
-            collect(Storage::files('public/' . $machine->device))
-                ->sortByDesc(function ($file) {
-                    return pathinfo($file, PATHINFO_FILENAME);
-                })
-            , 10);
+        // $files = $this->paginate(
+        //     collect(Storage::files('public/' . $machine->device))
+        //         ->sortByDesc(function ($file) {
+        //             return pathinfo($file, PATHINFO_FILENAME);
+        //         })
+        //     , 10);
+        $files = $this->paginate(Storage::files('public/' . $machine->device), 10);
         return view('admin.pages.machine.debug', compact('machine', 'files'));
     }
 
