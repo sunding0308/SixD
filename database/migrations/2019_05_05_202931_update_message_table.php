9c8ddc7bb8ec45e6ce016ba397e6fbddf9b49190
dong bin jiang <jiang.db@gmail.com>
2019-05-06 11:01:40 +0800

    Update message table

---
+++ database/migrations/2019_05_05_202931_update_message_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->integer('device_name')->unsigned()->index()->after('id');
+            $table->dropSoftDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('device_name');
+            $table->softDeletes();
+        });
+    }
+}
