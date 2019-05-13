c2d846dbbc5d18758602e6758a921c372965d4fd
dong bin jiang <jiang.db@gmail.com>
2019-05-13 11:34:44 +0800

    Set log permission to 0666

--- config/logging.php
+++ config/logging.php
@@ -49,6 +49,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 7,
+            'permission' => 0666
         ],
 
         'slack' => [
